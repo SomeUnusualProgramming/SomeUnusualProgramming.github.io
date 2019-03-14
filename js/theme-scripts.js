8fe02299345dc394afd3dd43e00bd32b0947e97c
SomeUnusualProgramming <swierkosz.adam@gmail.com>
2019-03-14 14:42:20 +0100

    Update theme-scripts.js

--- js/theme-scripts.js
+++ js/theme-scripts.js
@@ -6,22 +6,6 @@ $(window).scroll(function() {
 	}
 });
 
-// preloader
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
 $(function() {
 	$('a[href*=#]:not([href=#])').click(
 			function() {
